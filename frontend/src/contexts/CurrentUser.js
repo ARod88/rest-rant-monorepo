a901a681444e366fc79daad3f5e5c3f84b4faea1
ARod88 <alma.rodriguez72@yahoo.com>
2023-07-28 22:38:04 -0700

    update branch

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -3,18 +3,15 @@ import { createContext, useState, useEffect } from "react";
 export const CurrentUser = createContext();
 
 function CurrentUserProvider({ children }) {
+    
     const [currentUser, setCurrentUser] = useState(null);
-
-    window.setCurrentUser = setCurrentUser;
-
     useEffect(() => {
         const getLoggedInUser = async () => {
             let response = await fetch(
-                "http://localhost:5000/authentication/profile",
-                {
-                    credentials: "include",
-                }
-            );
+                'http://localhost:5000/authentication/profile/',{
+                    credentials: 'include'
+                })
+            
             let user = await response.json();
             if (response.ok) {
                 setCurrentUser(user);
--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -17,6 +17,16 @@ function LoginForm() {
 
     async function handleSubmit(e) {
         e.preventDefault()
+        const response = await fetch('http://localhost:5000/authentication', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        });
+
+        const data = await response.json()
        
 
     }
